7e2b476d7077c2b7b9c821f5bee897b09a4a9734
DavidCastrw <115431245+DavidCastrw@users.noreply.github.com>
2022-10-09 16:08:39 -0600

    Add files via upload

---
+++ 03_Math/dotNetMath/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.WriteLine("Math rlz!");
+
+//Suma +
+Console.WriteLine("\nSuma");
+Console.WriteLine(1 + 3);
+Console.WriteLine(16.8f + 1.4f);
+Console.WriteLine(19.8d + 3.1d);
+Console.WriteLine(17.3m + 1.1m);
+
+//Resta
+Console.WriteLine("\nResta");
+Console.WriteLine(5 - 2);
+Console.WriteLine(16.8f - 1.4f);
+Console.WriteLine(19.8d - 3.1d);
+Console.WriteLine(17.3m - 1.1m);
+
+//Multiplicacion *
+Console.WriteLine("\nMultiplicacion");
+Console.WriteLine(2 * 3);
+Console.WriteLine(2.5 * 3);
+Console.WriteLine(16.8f * 1.4f);
+Console.WriteLine(19.8d * 3.1d);
+Console.WriteLine(17.3m * 1.1m);
+
+//Division /
+Console.WriteLine("\nDivision");
+Console.WriteLine(16 / 3);
+Console.WriteLine(16 / 2.5);
+Console.WriteLine(16.8f / 1.4f);
+Console.WriteLine(19.8d / 3.1d);
+Console.WriteLine(17.3m / 1.1m);
+
+// Module %
+Console.WriteLine("\nModulo");
+Console.WriteLine( 5 % 4);
+Console.WriteLine(8 % 4);
+Console.WriteLine(-5 % 4);
+
+// Comparadores <> ==
+Console.WriteLine("\nComparadores");
+Console.WriteLine(8 > 4);
+Console.WriteLine(3 > 4);
+Console.WriteLine(1 < 2);
+Console.WriteLine(2 < 1);
+Console.WriteLine(5 == 5);
+Console.WriteLine(6 == 5);
---
+++ 04_Variables/variables/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Variables!");
+
+//Declaraciones
+Console.WriteLine("Declaraciones!");
+int age = 37;
+char initial = 'E';
+string lastName = "\n Gonzalez";
+decimal score = 99.99M;
+double grade = 45.0D;
+float distance = 5647.6547F;
+
+Console.WriteLine(lastName);
+Console.WriteLine(age);
+Console.WriteLine(initial);
+Console.WriteLine(score);
+Console.WriteLine(grade);
+Console.WriteLine(distance);
+
+//Re-asignaciones
+Console.WriteLine("Re-asignaciones!");
+age = 38;
+initial = 'A';
+lastName = "Blanco";
+
+
+Console.WriteLine(age);
+Console.WriteLine(initial);
+Console.WriteLine(lastName);
+
+//Operaciones
+Console.WriteLine("Operaciones!");
+double JoseFScore = 80.0D;
+double DavidScore = 89.5D;
+double MarioScore = 73.33D;
+double JoseIScore = 65.0D;
+
+double average;
+
+double total = JoseFScore + JoseIScore + MarioScore + DavidScore;
+
+average = total / 4;
+
+Console.WriteLine(average);
