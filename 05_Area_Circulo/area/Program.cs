a25cd71551c8857e7787b03f3cb0d266b49f3800
DavidCastrw <115431245+DavidCastrw@users.noreply.github.com>
2022-10-12 01:54:56 -0600

    Add files via upload

---
+++ 05_Area_Circulo/area/Program.cs
@@ -0,0 +1,18 @@
+﻿//Comienzo
+Console.WriteLine("Area del circulo!");
+
+double pi = 3.1416;
+int radio = 30 * 2;
+
+
+Console.WriteLine(pi);
+Console.WriteLine(radio);
+
+Console.WriteLine("El area del circulo es: ");
+
+double circunferencia;
+
+circunferencia = pi * radio;
+
+Console.WriteLine(circunferencia);
+
