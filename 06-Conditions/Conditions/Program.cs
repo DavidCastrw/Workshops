1c5a21cb61d2da9f6cb3cc33466c0c9a6a12ede9
DavidCastrw <115431245+DavidCastrw@users.noreply.github.com>
2022-10-19 09:35:43 -0600

    Add files via upload

---
+++ 06-Conditions/Conditions/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Conditions!");
+
+bool gradeOverMinimum = true;
+bool enoughtAssitance = false;
+
+if(gradeOverMinimum & enoughtAssitance){
+    Console.WriteLine("Student passed");
+}else
+    {
+        Console.WriteLine("Student did Not passed");
+    }
+
+
+Console.WriteLine("End of execution");
+
