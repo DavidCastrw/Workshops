8fd6e5b912cfede705dc7d7755db46d69a7590c5
DavidCastrw <115431245+DavidCastrw@users.noreply.github.com>
2022-10-19 09:38:18 -0600

    Add files via upload

---
+++ 05-EvenOdd/EvenOdd/Program.cs
@@ -0,0 +1,17 @@
+﻿string salutation ="Even Odd numbers!";
+Console.WriteLine(salutation);
+
+Console.WriteLine("Inserte un número");
+string input = Console.ReadLine() ?? "0";
+int number = Int32.Parse(input);
+
+if(number % 2 == 0){
+   Console.WriteLine("Es par!"); 
+}
+
+if(number % 2 == 1){
+    Console.WriteLine("Es impar!");
+}
+
+Console.WriteLine("Fin del programa..");
+
