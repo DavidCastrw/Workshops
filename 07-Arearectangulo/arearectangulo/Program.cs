efd351764de94b3b3bba853e5b29f08f42643334
DavidCastrw <115431245+DavidCastrw@users.noreply.github.com>
2022-10-19 23:52:44 -0600

    Add files via upload

---
+++ 07-Arearectangulo/arearectangulo/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Area del rectangulo!");
+
+int Area = 0;
+int Base = 0;
+int Hight = 0;
+
+Area = Base * Hight;
+string? input;
+
+Console.Write("Enter base: ");
+input = Console.ReadLine();
+Base = Convert.ToInt32(input);
+Console.Write("Enter the hight: ");
+input = Console.ReadLine();
+Hight = Convert.ToInt32(input);
+
+Console.Write("The area of the rectangule is: ");
+
+Area = Convert.ToInt32(input);
