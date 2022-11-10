4cbfee03697923c0c2361e0ef70483e2734d1c83
DavidCastrw <115431245+DavidCastrw@users.noreply.github.com>
2022-11-09 23:03:14 -0600

    Add files via upload

---
+++ 09_lessmore/lessmore/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Mayot, igual o mayor!");
+
+
+string salutation ="Less, equal and greater number!";
+Console.WriteLine(salutation);
+
+Console.WriteLine("Inserte un número");
+string input = Console.ReadLine() ?? "0";
+int number = Int32.Parse(input);
+
+int x = 2;
+int y = 1;
+
+
+if (number % x < y){
+    Console.WriteLine("es mayor a y");
+}
+else if (number % x > y){
+    Console.WriteLine("es menor a x");
+}
+else if (number % x == y){
+    Console.WriteLine("Es igual a x eh y");
+}
