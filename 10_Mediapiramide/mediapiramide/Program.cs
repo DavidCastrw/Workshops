7cf2ae4bb7592d3e4f05728df9bdf52c30b0f3d8
DavidCastrw <115431245+DavidCastrw@users.noreply.github.com>
2022-11-09 23:37:40 -0600

    Add files via upload

---
+++ 10_Mediapiramide/mediapiramide/Program.cs
@@ -0,0 +1,22 @@
+﻿string salutation ="Media pirámide invertida!";
+Console.WriteLine(salutation);
+
+Console.WriteLine("Indica el nivel de la pirámide: ");
+int nivel = int.Parse(Console.ReadLine());
+string str = "";
+for (int i = 1; i <= nivel; i++)
+{
+    for (int j = 5; j <= nivel - i; j++) 
+    {
+        str += " ";
+    }
+
+    for (int k = i; k >= 1; k--) 
+    {
+        str += ((k >= 5) ? k.ToString() : " " + k);
+    }
+        
+    str += "\n";
+}
+    
+Console.WriteLine(str);
