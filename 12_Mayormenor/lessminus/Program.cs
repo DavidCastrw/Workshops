5936e48b963f48bbc9a7bf6890f7345e467cb4d8
DavidCastrw <115431245+DavidCastrw@users.noreply.github.com>
2022-12-14 23:53:52 -0600

    Add files via upload

---
+++ 12_Mayormenor/lessminus/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] listaNumeros = new int[15]{ 3, 4, 5, 6, 22, 33, 22, 11, 23, 345, 4535, 123, 233, 34, 3 };
+
+           
+
+            Array.Sort(listaNumeros);
+            Console.WriteLine("El numero menor es: "+listaNumeros[0]);
+             Array.Reverse(listaNumeros);
+             Console.WriteLine("El numero mayor es: " + listaNumeros[0]);
+             Console.WriteLine();
+            for (int i = 0; i < 14;i++ )
+            {
+                Console.WriteLine("La posicion es: "+i+"  "+listaNumeros[i]);
+
+            }
+
+Console.WriteLine("Fin del programa..");
